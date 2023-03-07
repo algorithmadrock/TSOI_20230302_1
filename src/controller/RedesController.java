b48653fa185b565a7131981161ec0a4d18af6e68
luiza <luizamfelix@gmail.com>
2023-03-07 15:50:37 -0300

    Atualização 07/03/2023

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -34,41 +34,43 @@ public class RedesController {
 		if (SO.contains("Windows")) {
 			lerip("ipconfig");
 		} else { // é Linux
+			lerip("ifconfig");
 
 		}
 
 	}
 
 	private void lerip(String processo) {
-		//esse vai fazer a leitura do IP
-		
+		// esse vai fazer a leitura do IP
+
 		try {
 			Process ip = Runtime.getRuntime().exec(processo);
 
-			InputStream fluxo = ip.getInputStream();//lê as linhas do IP em bits
+			InputStream fluxo = ip.getInputStream();// lê as linhas do IP em bits
 			InputStreamReader leitor = new InputStreamReader(fluxo); // converte o fluxo de bits para algo legível
-			BufferedReader buffer = new BufferedReader(leitor); //vai ler as linhas de bit traduzidas com as informações fornecidas pelo processo filho
+			BufferedReader buffer = new BufferedReader(leitor); // vai ler as linhas de bit traduzidas com as
+																// informações fornecidas pelo processo filho
 
 			String linha = buffer.readLine();
-			while (linha != null) {
-				if(linha.contains("Adaptador")) {
-					
+			 while (linha != null) {
+				if (linha.contains("Adaptador")) {
+//					a criação do buffer depende da linha conter a variável indicada.
 					StringBuffer adaptador = new StringBuffer();
-					adaptador.append(linha); //salvo o nome do adaptador para mostrar
-				
+					adaptador.append(linha); // salvo o nome do adaptador para mostrar
 					linha = buffer.readLine();
 
-//					aqui estou lendo as linhas e colocando tudo no buffer, para depois analisá-las
-					while(linha.contains("Adaptador")== false) {
-						adaptador.append("\n"+linha);
-						linha = buffer.readLine();	
+//					aqui estou lendo as linhas e verificando se não mudei de adaptador, se o adaptador tiver "IPv4", o código mostra ele
+					while (linha.contains("Adaptador") == false) {
+						if (linha.contains("IPv4")) {
+							adaptador.append("\n" + linha);
+							System.out.println(adaptador);
+						}
+						linha = buffer.readLine();
 					}
-					
-					String adapt = adaptador.toString();
-					if (adapt.contains("IPv4")) {
-						System.out.println(adaptador);
-					}
+				}else { // sempre vai aparecer a mensagem de erro pq a linha dá "null" na última "rodada"
+				linha = buffer.readLine();
 				}
+
 			}
 			buffer.close();
 			leitor.close();
@@ -77,8 +79,46 @@ public class RedesController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 
 	}
 
+//	3)
+	public void ping() {
+
+		String SO = os();
+		if (SO.contains("Windows")) {
+			lerping("PING -4 -n 10 www.google.com.br");
+		} else { // é Linux
+			lerping(": PING -4 -c 10 www.google.com.br");
+		}
+
+	}
+
+	private void lerping(String processo) {
+		
+		try {
+			Process ping = Runtime.getRuntime().exec(processo);
+
+//		transformação no fluxo de bits em algo legível
+			InputStream fluxo = ping.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+
+			String linha = buffer.readLine();
+
+			while (linha != null) {
+				if (linha.contains(" M�dia = ")) {
+					String[] palavra = linha.split("M�dia = ");
+					System.out.println("PING Médio: "+palavra[1]);
+					}
+				linha = buffer.readLine();	
+			} 
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+	
+		}catch(IOException e){
+		e.printStackTrace();
+		}
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -6,14 +6,34 @@ DATA        : 02/03/2023
 
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.RedesController;
 
 public class Main {
 
 	public static void main(String[] args) {
 		RedesController redes = new RedesController();
-		
-		redes.ip();
+		int opcao = 0;
+
+		do {
+			opcao = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma configuração para ser acessada:\n 2) Redes IPv4\n 3) PING Médio\n Para SAIR digite 0."));
+			switch (opcao) {
+			case 2:
+				redes.ip();
+				break;
+			case 3:
+				redes.ping();
+				break;
+
+			case 0:
+				System.out.println("Programa finalizado com sucesso! ");
+				break;
+			default:
+				System.out.println("!!! ERRO: Digite uma opção válida!!!");
+			}
+
+		} while (opcao != 0);
 
 	}
 
